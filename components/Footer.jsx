e173a988476f9e87a265c496e52f728cbaf9a2ce
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-08-30 13:26:49 +0400

    First commit

---
+++ components/Footer.jsx
@@ -0,0 +1,4 @@
+export function Footer()
+{
+    return <footer>(c) 2023</footer>
+}
---
+++ components/Header.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+import styles from './Header.module.css'
+
+const pages = [
+    {href: '/', name: 'Home'},
+    {href: '/about', name: 'About'}
+]
+
+export function Header() {
+    return <header>
+        <nav className={styles.nav}>
+            <ul>
+                {pages.map(({href, name})=>
+                <li key = {href}><Link href={href}>{name}</Link></li>)}
+            </ul>
+        </nav>
+    </header>
+}
